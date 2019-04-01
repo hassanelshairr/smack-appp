e2b96caf5e8d0f7464e5920f492c17279ce7bc05
hassan elshaer <hassanelshaer@hassans-MacBook-Pro.local>
2019-04-01 15:25:50 +0200

    starter project

---
+++ Smack appp/Smack appp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack appp
+//
+//  Created by hassan elshaer on 4/1/19.
+//  Copyright © 2019 hassan elshaer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

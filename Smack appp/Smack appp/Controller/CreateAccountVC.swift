cf44344c3cbd72e085f814c26f9c20fc084721ee
hassan elshaer <hassanelshaer@hassans-MacBook-Pro.local>
2019-04-09 18:49:51 +0200

    second step

--- Smack appp/Smack appp/Controller/CreateAccountVC.swift
+++ Smack appp/Smack appp/Controller/CreateAccountVC.swift
@@ -10,12 +10,37 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    @IBOutlet weak var nametxt: UITextField!
+    @IBOutlet weak var emailtxt: UITextField!
+    @IBOutlet weak var passtxt: UITextField!
+    @IBOutlet weak var userimg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func pickbcbgColor(_ sender: Any) {
+    }
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    @IBAction func creataccbtn(_ sender: Any) {
+        
+        guard let email = emailtxt.text , emailtxt.text != "" else {return}
+        guard let pass = passtxt.text , passtxt.text != "" else {return}
+        
+        
+        
+        AuthService.instance.registerUser(email: email, password: pass)
+        { (success) in
+            if success {
+                print("registered user")
+            } 
+        }
+        
+    }
     
     @IBAction func ExitPressed(_ sender: Any) {
         
